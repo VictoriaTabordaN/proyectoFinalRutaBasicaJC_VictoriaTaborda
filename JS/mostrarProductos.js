5282c20c24d8c3c71c7f0caff5ee728a581cab25
Victoria Taborda <[vickytab13@gmail.com]>
2024-11-10 21:47:11 -0300

    Se creo la sesión productos, a la par se le agregaron estilos y se creo una funión para poder abrir cada categoría de productos en una pagina nueva.

---
+++ JS/mostrarProductos.js
@@ -0,0 +1,12 @@
+function mostrarAnillos(){
+    window.location.href = 'sesionAnillos.html';
+}
+
+function mostrarCaravanas(){
+    window.location.href = 'sesionCaravanas.html';
+}
+
+function mostrarCollares(){
+    window.location.href = 'sesionCollares.html';
+}
+
